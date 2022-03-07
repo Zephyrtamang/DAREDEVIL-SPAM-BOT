ff69082155b1098cd52d9d8e1db4a03938c44126
Zephyrtamang <101117620+Zephyrtamang@users.noreply.github.com>
2022-03-07 22:23:04 +0545

    Update SPAM-X-BOT.py

--- SPAM-X-BOT.py
+++ SPAM-X-BOT.py
@@ -749,7 +749,7 @@ async def _(e):
 async def ping(e):
     if e.sender_id in SMEX_USERS:
         start = datetime.now()
-        text = "FUCK U😂"
+        text = "FUCK U"
         event = await e.reply(text, parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
